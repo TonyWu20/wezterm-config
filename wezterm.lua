288ccaec7e397ed95282729d75639ea3d32ffa27
TonyWu20 <tony.w21@gmail.com>
2023-05-22 15:20:42 +0800

    Use nerd font 3.0 Hack Nerd Font Mono

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,20 @@
-local wezterm = require 'wezterm';
-local catppuccin = require("colors/catppuccin").setup {
-    flavour = "mocha"
-}
+local wezterm = require("wezterm")
+local gruvbox_dark = wezterm.get_builtin_color_schemes()["Gruvbox Dark"]
 return {
-    font = wezterm.font("JetBrainsMono Nerd Font Mono" ),
-    font_size = 18,
-    line_height = 1.0,
-    -- color_scheme = "catppuccin",
-    colors = catppuccin,
-    window_decorations = "RESIZE"
+	-- font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+	font = wezterm.font("Hack Nerd Font Mono"),
+	front_end = "WebGpu",
+	window_background_opacity = 1,
+	font_size = 18,
+	line_height = 1.0,
+	color_schemes = {
+		["Gruvbox Dark"] = gruvbox_dark,
+	},
+	-- color_scheme = "Gruvbox Dark",
+	color_scheme = "Catppuccin Mocha",
+	window_decorations = "RESIZE",
+	max_fps = 120,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = true,
+	tab_bar_at_bottom = true,
 }
