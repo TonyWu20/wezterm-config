043abaa0bab602be7eaae6b8e87dbef690af02d2
TonyWu20 <tony.w21@gmail.com>
2024-05-31 10:56:40 +0800

    Zen mode of nvim

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,33 @@
 local wezterm = require("wezterm")
 local gruvbox_dark = wezterm.get_builtin_color_schemes()["Gruvbox Dark"]
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" and not overrides.line_height then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+			overrides.line_height = 1.5
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+			overrides.line_height = 1.5
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+			overrides.line_height = 1.5
+		end
+	else
+		overrides.line_height = nil
+	end
+	window:set_config_overrides(overrides)
+end)
 return {
 	-- font = wezterm.font("JetBrainsMono Nerd Font Mono"),
 	font = wezterm.font_with_fallback({ "Hack Nerd Font", "Symbols Nerd Font" }),
@@ -17,26 +45,4 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = true,
 	tab_bar_at_bottom = true,
-	wezterm.on("user-var-changed", function(window, pane, name, value)
-		local overrides = window:get_config_overrides() or {}
-		if name == "ZEN_MODE" then
-			local incremental = value:find("+")
-			local number_value = tonumber(value)
-			if incremental ~= nil then
-				while number_value > 0 do
-					window:perform_action(wezterm.action.IncreaseFontSize, pane)
-					number_value = number_value - 1
-				end
-				overrides.enable_tab_bar = false
-			elseif number_value < 0 then
-				window:perform_action(wezterm.action.ResetFontSize, pane)
-				overrides.font_size = nil
-				overrides.enable_tab_bar = true
-			else
-				overrides.font_size = number_value
-				overrides.enable_tab_bar = false
-			end
-		end
-		window:set_config_overrides(overrides)
-	end),
 }
